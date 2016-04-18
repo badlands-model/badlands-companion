b1366d628064269e1a967c2e2a251fb652b48b1d
Ian Howson <ian@mutexlabs.com>
2016-04-18 11:37:31 +1000

    Convert to a pip-installable package. Update notebooks to suit the new package paths.

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+"""
+setup.py for pyBadlands Companion
+"""
+
+setup(
+    name="pybadlands_companion",
+    version="0.1",
+    author="Tristan Salles",
+    author_email="tristan.salles@sydney.edu.au",
+    description=("Pre and post processing scripts for pyBadlands"),
+    url='https://github.com/badlands-model/pyBadlands-Companion',
+    long_description=open('README.md').read(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        'Programming Language :: Python :: 2.7',
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+    ],
+    packages=['pybadlands_companion'],
+    install_requires=[
+        'pyBadlands'
+    ]
+)
